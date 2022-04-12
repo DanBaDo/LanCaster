9f725fd518e199812b951c92a2f47652113afdae
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-04-12 19:13:03 +0200

    Cleanup

--- PoC/frontend/src/script.mjs
+++
@@ -1,5 +0,0 @@
-console.log("Here I'm!");
-
-const local = new RTCPeerConnection()
-//local.setLocalDescription() // For updating local conection parameters
-api.sessions.post(local.localDescription)
